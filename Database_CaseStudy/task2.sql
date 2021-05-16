28299478a348f9750a71f827e34de1bad52c0f5b
huynhduc296 <huynhduc290698@gmail.com>
2021-05-16 02:48:58 -1200

    commit

--- Database_CaseStudy/task2.sql
+++ Database_CaseStudy/task2.sql
@@ -6,6 +6,7 @@ from nhan_vien nv
 join bo_phan bp on nv.id_bo_phan = bp.id_bo_phan
 join vi_tri vt on nv.id_vi_tri = vt.id_vi_tri
 join trinh_do td on nv.id_trinh_do= td.id_trinh_do
-where nv.name_nhan_vien regexp '[H][a-z]*$' or nv.name_nhan_vien regexp '[K][a-z]*$' or  nv.name_nhan_vien regexp '[T][a-z]*$'
+where nv.name_nhan_vien regexp '[H][a-z]*$' or nv.name_nhan_vien regexp '[K][a-z]*$'
+ or  nv.name_nhan_vien regexp '[T][a-z]*$'and length(name_nhan_vien) <=15
 order by nv.id_nhan_vien
 
