2a09c6e99e5831e81aae89dbc222b1dcf39fa20a
huynhduc296 <huynhduc290698@gmail.com>
2021-05-17 20:24:29 -1200

    commit

--- Database_CaseStudy/task5.sql
+++ Database_CaseStudy/task5.sql
@@ -5,10 +5,11 @@
 
 select kh.id_khach_hang , kh.ngay_sinh, lkh.name_loai_khach_hang, 
 		hd.id_hop_dong , dv.name_dich_vu, hd.ngay_lam_hop_dong ,
-        hd.ngay_het_hop_dong, dv.chi_phi_thue + (hdct.so_luong * dvdk.gia_ban) as tong_tien
+        hd.ngay_het_hop_dong, sum(dv.chi_phi_thue + (hdct.so_luong * dvdk.gia_ban)) as tongtien
 	from khach_hang kh 
 	left join hop_dong hd on kh.id_khach_hang = hd.id_khach_hang
 	left join loai_khach_hang lkh on lkh.id_loai_khach_hang=kh.id_loai_khach_hang
 	left join dich_vu dv on dv.id_dich_vu  =hd.id_dich_vu
 	left join hop_dong_chi_tiet hdct on hd.id_hop_dong=hdct.id_hop_dong
-	left join dich_vu_di_kem dvdk on dvdk.id_dich_vu_di_kem= hdct.id_dich_vu_di_kem;
+	left join dich_vu_di_kem dvdk on dvdk.id_dich_vu_di_kem= hdct.id_dich_vu_di_kem
+    group by id_khach_hang
