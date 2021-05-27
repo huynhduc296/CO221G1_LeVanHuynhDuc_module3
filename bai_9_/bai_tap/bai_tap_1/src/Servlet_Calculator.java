cf8760b06ca920431c6a5aafaa97076be0c9f329
huynhduc296 <huynhduc290698@gmail.com>
2021-05-26 19:33:14 -1200

    commit

--- bai_9_/bai_tap/bai_tap_1/src/Servlet_Calculator.java
+++ bai_9_/bai_tap/bai_tap_1/src/Servlet_Calculator.java
@@ -1,12 +1,12 @@
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
-@WebServlet(name = "Servlet_Calculator")
+@WebServlet(name = "Servlet_Calculator",urlPatterns = "/calculate")
 public class Servlet_Calculator extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String product = request.getParameter("product");
@@ -16,14 +16,9 @@ public class Servlet_Calculator extends HttpServlet {
         float discountAmount = price * discount / 100;
         float discountPrice = price - discountAmount;
 
-        HttpSession session = request.getSession();
-        session.setAttribute("product", product);
-        session.setAttribute("price", price);
-        session.setAttribute("discount", discount);
-        session.setAttribute("discountAmount", discountAmount);
-        session.setAttribute("discountPrice", discountPrice);
-
-        request.getRequestDispatcher("show_discount.jsp").forward(request, response);
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("discount.jsp");
+        request.setAttribute("sum1",discountPrice);
+        requestDispatcher.forward(request,response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
